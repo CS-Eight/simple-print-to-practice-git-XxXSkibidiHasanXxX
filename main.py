86fba6ae4c2542d9a32cf935c76d9ad3605da811
XxXSkibidiHasanXxX <hgokce@paedu.org>
2025-12-04 19:32:19 +0000

    done

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Welcome to Python!
 # This is your first Python program
 
-# The print() function displays text on the screen
-print("Hello, World!")
+# The print() functionap9sfgas8d07gnfs8a9dtna98segn8s9dg asdv7f displays text on the screen
+print("Hello Pinnacle")
 
 # Try printing your own message below:
 # print("Your message here")
